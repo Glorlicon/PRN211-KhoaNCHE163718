7e8b6f480adddc0762b1aaf043c7fe6c1520a509
Glorlicon <khoabk1234@gmail.com>
2022-06-02 01:46:59 +0700

    Added Slot 10 work

---
+++ Week-4/Slot 10/System.Threading.Thread Class/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using static System.Console;
+
+namespace System.Threading.Thread_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread primaryThread = Thread.CurrentThread;
+            primaryThread.Name = "ThePrimaryThread";
+            WriteLine($"ID of current thread: { primaryThread.ManagedThreadId}");
+            WriteLine($"Thread Name: {primaryThread.Name}");
+            WriteLine($"Has thread started?: {primaryThread.IsAlive}");
+            WriteLine($"Priority Level: {primaryThread.Priority}");
+            WriteLine($"Thread State: {primaryThread.ThreadState}");
+            ReadLine();
+        }
+    }
+}
---
+++ Week-4/Slot 10/Timer Callbacks/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace Timer_Callbacks
+{
+    
+    internal class Program
+    {
+        static void PrintTime(object state)
+        {
+            Console.WriteLine("Time is: {0}. Pram is {1}",
+            DateTime.Now.ToLongTimeString(), state.ToString());
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Working with Timer type *****");
+            TimerCallback timeCB = new TimerCallback(PrintTime);
+
+            var _ = new Timer(
+                timeCB,
+                "Hello from Main",
+                0,
+                1000);
+            Console.ReadLine();
+        }
+    }
+}
