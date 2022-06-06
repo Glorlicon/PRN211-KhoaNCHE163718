fabba206b4371cae95ec41c86279cd00bcd33f3c
Glorlicon <khoabk1234@gmail.com>
2022-06-06 20:21:29 +0700

    Added work for slot 11 and 12

---
+++ Week-4/Slot 11/MyConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static MyLibrary.MyClass;
+
+namespace MyConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 50, b = 52;
+            int result;
+            Console.WriteLine("***** Demo Consuming Assemblies *****");
+            result = a.Add(b);
+            Console.WriteLine($"{a}+{b} = {result}");
+            result = a.Sub(b);
+            Console.WriteLine($"{a}-{b} = {result}");
+            Console.ReadLine(); 
+        }
+    }
+}
